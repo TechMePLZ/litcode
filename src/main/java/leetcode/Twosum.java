24c1f4389d06557144549f5a09c42a38686985d1
TeachMePLZ <sfgraff@yandex.ru>
2023-04-15 20:01:11 +0300

    leetcode

--- src/main/java/leetcode/Twosum.java
+++ src/main/java/leetcode/Twosum.java
@@ -1,33 +1,47 @@
 package leetcode;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
- *
+ * <p>
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
- *
+ * <p>
  * You can return the answer in any order.
- *
- *
- *
+ * <p>
  * Example 1:
- *
+ * <p>
  * Input: nums = [2,7,11,15], target = 9
  * Output: [0,1]
  * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
- * Example 2:
- *
- * Input: nums = [3,2,4], target = 6
- * Output: [1,2]
- * Example 3:
- *
- * Input: nums = [3,3], target = 6
- * Output: [0,1]
+ * Решение через хэшмап где ключ  значение в массиве, а индекс как значение
  */
 public class Twosum {
     public static void main(String[] args) {
 
+        int[] nums = {2,6,5,7};
+        int target = 9;
+        int[] sol = solution(nums, target);
 
-
-
+        System.out.println(Arrays.toString(sol));
     }
+    public static int[] solution(int[] nums, int target){
+        int n = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] result = new int[2];
+        for (int i = 0; i < n; i++) {
+            if (map.containsKey(target - nums[i])) {
+                result[1] = i;
+                result[0] = map.get(target - nums[i]);
+                return result;
+            }
+            map.put(nums[i], i);
+        }
+        return result;
+    }
+
+
+
 }
